d35dec64e6bcc5d08ea538d18ac2804eb53972b0
Mia Bramel <mab2400@columbia.edu>
2020-12-19 01:55:48 +0000

    Changed port 443 to 10834

--- server.cpp
+++ server.cpp
@@ -12,7 +12,7 @@
 #include <openssl/err.h>
 
 #define  PASS_PORT  25565   // client auth using username/password
-#define  CERT_PORT  443     // client auth using certificate
+#define  CERT_PORT  10834     // client auth using certificate
 
 #define  BUFSIZE    4096
 
