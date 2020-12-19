55f027cbc815aeb667acb7f5803920b25d781323
Mia Bramel <mab2400@columbia.edu>
2020-12-19 18:01:59 -0500

    Changed setup.sh to comply with Bellovin's solutions

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 rm -rf ../rootca
 make clean
 make
-./boilerplate/openssl.sh
-./boilerplate/server.sh
-./boilerplate/client.sh
+./BellovinHW2Solutions/gen-ca-certs.sh
+./BellovinHW2Solutions/gen-server-cert.sh
+./BellovinHW2Solutions/gen-client-key.sh
 valgrind ./server
