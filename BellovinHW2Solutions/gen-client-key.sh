89201e8c831bc0a87e8e13b83c4c84e5892db919
Mia Bramel <mab2400@columbia.edu>
2020-12-21 17:49:54 +0000

    Separated public and private keys

--- BellovinHW2Solutions/gen-client-key.sh
+++ BellovinHW2Solutions/gen-client-key.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-export CLIENT_PASS="topsecretclientpassword"
+# generate web client public/private key pair 
 
- # generate web client certificate
- cd certs/ca
- openssl genpkey -out client/private/client.key.pem -outform PEM -pass env:CLIENT_PASS -aes256 -algorithm RSA -pkeyopt rsa_keygen_bits:2048
- pwd
- chmod 400 client/private/client.key.pem
+cd certs/ca
+
+# Get the private and public keys
+openssl genrsa -out client/client-priv.key.pem -aes256 2048 
+openssl rsa -in client/client-priv.key.pem -pubout -out client/client-pub.key.pem -aes256 2048 
+#chmod 400 client/client-priv.key.pem
+#chmod 400 client/client-pub.key.pem
+
+# Using the private key to create a certificate signing request (CSR).
+openssl req -config intermediate/openssl-inter2.cnf \
+            -new -sha256 -out intermediate/csr/client.csr.pem \
+            -key client/client-priv.key.pem \
+
+chmod 400 client/client-priv.key.pem
+chmod 400 client/client-pub.key.pem
