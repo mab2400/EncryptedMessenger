87d94f08df627d896f768c355d0ca36523c6ca8f
Mia Bramel <mab2400@columbia.edu>
2020-12-22 19:50:44 +0000

    gen-client-keys-and-csr.sh removes the keys after they are generated (no longer necessary)

---
+++ BellovinHW2Solutions/gen-client-keys-and-csr.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# generate web client public/private key pair 
+export COMMON_NAME=$1
+export CLIENT_PASS=$2
+
+cd certs/ca
+
+# Get the private and public keys
+openssl genrsa -passout env:CLIENT_PASS -out client/client-priv.key.pem -aes256 2048 
+
+openssl rsa -passin env:CLIENT_PASS -in client/client-priv.key.pem -pubout -out client/client-pub.key.pem -aes256 2048 
+
+#chmod 400 client/client-priv.key.pem
+#chmod 400 client/client-pub.key.pem
+echo "/C=US/ST=New York/O=COMS4181 Hw2/CN={$1}"
+
+# Using the private key to create a certificate signing request (CSR).
+openssl req -config intermediate/openssl-inter.cnf \
+            -new -sha256 -out intermediate/csr/client.csr.pem \
+            -key client/client-priv.key.pem \
+	    -passout env:CLIENT_PASS \
+	    -subj "/C=US/ST=New York/O=COMS4181 Hw2/CN={$1}"
+
+rm client/client-pub.key.pem
+rm client/client-priv.key.pem
