edfc8e6333168fd06ea375ce9474d682e3724ada
Mia Bramel <mab2400@columbia.edu>
2020-12-24 23:33:37 +0000

    Changed to usr_cert

--- server/gen-client-cert.sh
+++ server/gen-client-cert.sh
@@ -3,6 +3,6 @@
 # Take the CSR sent by the server and use it to create and sign a certificate 
 
 openssl ca -config openssl-inter.cnf \
-           -extensions encryption_cert -notext -md sha256 \
+           -extensions usr_cert -notext -md sha256 \
            -in users/$1/csr_temp.pem \
            -out users/$1/cert
