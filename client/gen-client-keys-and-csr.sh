beec82d44c5c46c14e64a7205110e496b8e6e9eb
Michael Jan <michaeljancontact@gmail.com>
2020-12-25 19:51:20 +0000

    no more pkey passphrase

--- client/gen-client-keys-and-csr.sh
+++ client/gen-client-keys-and-csr.sh
@@ -5,14 +5,13 @@ export COMMON_NAME=$1
 export CLIENT_PASS=$2
 
 # Get the private and public keys
-openssl genrsa -passout env:CLIENT_PASS -out $1-priv.key.pem -aes256 2048 
+openssl genrsa -out $1-priv.key.pem 2048 
 
-openssl rsa -passin env:CLIENT_PASS -in $1-priv.key.pem -pubout -out $1-pub.key.pem -aes256 2048 
+openssl rsa -in $1-priv.key.pem -pubout -out $1-pub.key.pem  2048 
 
 chmod 600 $1-priv.key.pem
 
 # Using the private key to create a certificate signing request (CSR).
 openssl req -new -sha256 -out $1.csr.pem \
             -key $1-priv.key.pem \
-	    -passout env:CLIENT_PASS \
 	    -subj "/C=US/ST=New York/O=COMS4181 Hw2/CN={$1}"
