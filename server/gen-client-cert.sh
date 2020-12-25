37f86cb2d8ee19e0a1e6e5cc6a2ef37e9bf6c67b
Mia Bramel <mab2400@columbia.edu>
2020-12-25 21:14:54 +0000

    Automated y/n option

--- server/gen-client-cert.sh
+++ server/gen-client-cert.sh
@@ -3,8 +3,9 @@
 # Take the CSR sent by the server and use it to create and sign a certificate 
 export INTER_PASS=$2
 
-openssl ca -config openssl-inter.cnf \
+openssl ca -batch -config openssl-inter.cnf \
            -extensions usr_cert -notext -md sha256 \
 	   -passin env:INTER_PASS \
            -in users/$1/csr_temp.pem \
            -out users/$1/cert
+
