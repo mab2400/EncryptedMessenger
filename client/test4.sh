9a09d10738dc09a1291d40a079a9ccadeeee4cd5
Mia Bramel <mab2400@columbia.edu>
2020-12-25 04:47:01 +0000

    Test 4. Haven't tested it yet lol

---
+++ client/test4.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+rm hi.txt
+rm addleness-cert.pem
+rm addleness-priv.key.pem
+rm unrosed-cert.pem
+rm unrosed-priv.key.pem
+
+make clean
+make
+
+valgrind ./getcert 127.0.0.1 addleness Cardin_pwns
+
+mv addleness-cert.pem addleness-temp-cert.pem
+
+valgrind ./getcert 127.0.0.1 unrosed shamed_Dow 
+
+# Generate a new cert 
+valgrind ./changepw 127.0.0.1 addleness Cardin_pwns newpass 
+
+mv addleness-cert.pem addleness-new-cert.pem
+
+# Saving the old cert into addleness-cert.pem (ensuring sendmsg will use the old cert instead of new one)
+mv addleness-temp-cert.pem addleness-cert.pem 
+
+echo "Hi there!" > hi.txt
+
+echo "addleness" | valgrind ./sendmsg 127.0.0.1 hi.txt unrosed
