f116a63062695935af3c982d54a08234b4ae0f27
Lucie le Blanc <lucieleblanc425@gmail.com>
2020-12-25 20:17:24 -0500

    Delete unused scripts

--- client/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-make clean
-make
-
-valgrind ./getcert localhost addleness Cardin_pwns
--- server/save-password.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd users/$1
-# Writes the password into a textfile within the username directory
-echo "${2}" > password.txt
